f1634986c68e84587f8e19a1804f2ffc0ecebf24
Virtao <virtao@virtao.org>
2015-09-10 16:06:34 +0800

    Update 004_ActivityLife.

--- 004_ActivityLife/app/src/main/java/org/virtao/activitylife/DialogActivity.java
+++ 004_ActivityLife/app/src/main/java/org/virtao/activitylife/DialogActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class DialogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
     }
--- 004_ActivityLife/app/src/main/java/org/virtao/activitylife/MainActivity.java
+++ 004_ActivityLife/app/src/main/java/org/virtao/activitylife/MainActivity.java
@@ -1,17 +1,63 @@
 package org.virtao.activitylife;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        Button startNormalActivity = (Button) findViewById(R.id.main_activity_start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.main_activity_start_dialog_activity);
+
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        EditText text4Save = (EditText) findViewById(R.id.main_activity_text_for_save);
+        if (savedInstanceState != null) {
+            String tmpStr = savedInstanceState.getString("text");
+            text4Save.setText(tmpStr);
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        EditText text4Save = (EditText) findViewById(R.id.main_activity_text_for_save);
+        outState.putString("text", String.valueOf(text4Save.getText()));
+        Log.d(TAG, "onSaveInstanceState");
+
     }
 
     @Override
@@ -34,5 +80,41 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestory");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
     }
 }
